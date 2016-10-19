acecaeb0ed33a1180b3a8f8709b9849b7e951446
brkt <brkt@kershado>
2016-10-19 15:49:54 +0300

    Including header file syntax error fixed. And stdint.h library included for uint8, uint16 typedefs.

--- emeter-toolkit/LCD-internal.c
+++ emeter-toolkit/LCD-internal.c
@@ -63,7 +63,7 @@
 //--------------------------------------------------------------------------
 #include <stdint.h>
 #include <stdlib.h>
-#include <io.h>
+#include "io.h"
 #include "emeter-toolkit-custom.h"
 #include "emeter-toolkit.h"
 
